0f353dafdac6f5f519e7137408283387a8a9eee6
benjaminr177 <unoceptik@gmail.com>
2017-09-29 16:06:21 +0300

    Multiple users can work on the homepage

--- public/script.js
+++ public/script.js
@@ -3,13 +3,32 @@ window.onload = () => {
   var pad = document.getElementById('pad');
   var markdownArea = document.getElementById('markdown');
 
+  var previousMarkdownValue;
+
   var convertTextAreaToMarkdown = () => {
     var markdownText = pad.value;
+    previousMarkdownValue = markdownText;
     html = converter.makeHtml(markdownText);
     markdownArea.innerHTML = html;
   }
 
+  var didChangeOccur = () => {
+    if (previousMarkdownValue != pad.value) {
+      return true;
+    }
+    return false;
+  }
+
+  setInterval(() => {
+    if (didChangeOccur()) {
+      convertTextAreaToMarkdown();
+    }
+  }, 1000);
+
   pad.addEventListener('input', convertTextAreaToMarkdown);
 
-  convertTextAreaToMarkdown();
+  sharejs.open('home', 'text', (error,doc) => {
+    doc.attach_textarea(pad);
+    convertTextAreaToMarkdown();
+  });
 }
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var sharejs = require('share');
+require('redis');
 
 // view engine to ejs -- maybe change this
 app.set('view engine', 'ejs');
@@ -10,7 +12,17 @@ app.get('/', (req, res) => {
   res.render('pad');
 });
 
-app.get('')
+app.get('/(:id)', (req,res) => {
+  res.render('pad');
+})
+
+var options = {
+  db: {
+    type:'redis'
+  },
+}
+
+sharejs.server.attach(app, options);
 
 var port = process.env.PORT || 8080;
 console.log("listening on port 8080");
